e3900f1c5e519663661986c0b61257c1e93553d4
Jason <jasonm322@gmail.com>
2023-03-30 15:36:21 -0700

    Created Test Class

---
+++ src/TestClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by JSpec on 3/30/2023.
+//
+
+#include "TestClass.h"
---
+++ src/TestClass.h
@@ -0,0 +1,14 @@
+//
+// Created by JSpec on 3/30/2023.
+//
+
+#ifndef MYPROJECT_TESTCLASS_H
+#define MYPROJECT_TESTCLASS_H
+
+
+class TestClass {
+
+};
+
+
+#endif //MYPROJECT_TESTCLASS_H
